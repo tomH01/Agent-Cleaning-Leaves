fd802c33b878a5673e2f072d432fa010430bbb77
tomH01 <tomth.hagel@gmail.com>
2023-01-31 11:20:49 +0100

    Final changes

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ def main():
     env = Grid(dimension=(15, 15), p_leaves=0.6, reward_conf=(-1, 5, -5, 100), max_moves=200, state_mode="nbh")
     ql = QLearning(env)
 
-    ql.training(epochs=500, alpha=0.01, gamma=0.8, epsilon=0.7 , print_interval=100, save=True, load=True)
+    ql.training(epochs=500, alpha=0.01, gamma=0.8, epsilon=0.7 , print_interval=100, save=True, load=False)
     ql.execute(ql.best_run)
 
 
